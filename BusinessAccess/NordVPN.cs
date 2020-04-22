55738a82f71b44ced8d600f958c116c4f799ebb7
pitiascp <congdiepc6@gmail.com>
2020-04-22 21:35:05 +0700

    #1 Create Class NordVPN

---
+++ BusinessAccess/NordVPN.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessAccess
+{
+    class NordVPN
+    {
+        public void RunCommandCMD(string command)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            Process cmd = new Process();
+            cmd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.Arguments = "/c " + command;
+            cmd.Start();
+            cmd.WaitForExit();
+        }
+        public void NordDisconnect()
+        {
+            RunCommandCMD("\"C:\\Program Files (x86)\\NordVPN\\NordVPN.exe\" -d");
+        }
+        public void NordResetIP()
+        {
+            RunCommandCMD("\"C:\\Program Files (x86)\\NordVPN\\NordVPN.exe\" -changeip");
+        }
+        public void NordChangeRegion(string region)
+        {
+            string cmd = "\"C:\\Program Files (x86)\\NordVPN\\NordVPN.exe\"  -c -g" + region;
+            //Chỗ này làm thêm cái check IP, change thành công rồi mới xong
+            RunCommandCMD(cmd);
+        }
+    }
+}
